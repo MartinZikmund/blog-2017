b053115aa2806370e8efea9b4aa9d8b18ddee49a
Martin Zikmund <martinzikmund@live.com>
2017-01-02 11:25:14 +0100

    UWPCustomFonts - Example OTF font added

--- UWPCustomFonts/UWPCustomFonts/MainPage.xaml.cs
+++ UWPCustomFonts/UWPCustomFonts/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -24,7 +25,7 @@ namespace UWPCustomFonts
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();            
             SetFontFromCode();
         }
 
