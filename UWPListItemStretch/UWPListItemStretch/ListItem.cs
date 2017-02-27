a949397d21067bb96efac0fda18ab04a5fb68065
Martin Zikmund <martinzikmund@live.com>
2017-02-27 14:43:42 +0100

    UWP List Item Stretch

---
+++ UWPListItemStretch/UWPListItemStretch/ListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace UWPListItemStretch
+{
+    public class ListItem
+    {
+        public ListItem( Color color, string text )
+        {
+            Color = color;
+            Text = text;
+        }
+
+        public Color Color { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ UWPListItemStretch/UWPListItemStretch/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPListItemStretch
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public ObservableCollection<ListItem> SampleItems { get; } = new ObservableCollection<ListItem>()
+        {
+            new ListItem(Colors.Red, "Red text"),
+            new ListItem(Colors.Blue, "Blue text"),
+            new ListItem(Colors.Green, "Green text"),
+            new ListItem(Colors.BlueViolet, "Violet text"),
+        };
+    }
+}
