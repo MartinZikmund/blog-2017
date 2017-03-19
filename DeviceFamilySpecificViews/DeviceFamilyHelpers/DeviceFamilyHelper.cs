26df3ebe28d04a58a2cd60565023cc44afcaf54b
Martin Zikmund <martinzikmund@live.com>
2017-03-19 10:56:45 +0100

    All device families reflected in the view

--- DeviceFamilySpecificViews/DeviceFamilyHelpers/DeviceFamilyHelper.cs
+++ DeviceFamilySpecificViews/DeviceFamilyHelpers/DeviceFamilyHelper.cs
@@ -7,37 +7,37 @@ using System.Threading.Tasks;
 namespace DeviceFamilyHelpers
 {
 
-    /// <summary>
-    /// Retrieves strongly-typed device family
-    /// </summary>
-    public static class DeviceFamilyHelper
+/// <summary>
+/// Retrieves strongly-typed device family
+/// </summary>
+public static class DeviceFamilyHelper
+{
+    static DeviceFamilyHelper()
     {
-        static DeviceFamilyHelper()
-        {
-            DeviceFamily = RecognizeDeviceFamily(Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily);
-        }
-
-        private static DeviceFamily RecognizeDeviceFamily(string deviceFamily)
-        {
-            switch (deviceFamily)
-            {
-                case "Windows.Mobile":
-                    return DeviceFamily.Mobile;
-                case "Windows.Desktop":
-                    return DeviceFamily.Desktop;
-                case "Windows.Xbox":
-                    return DeviceFamily.Xbox;
-                case "Windows.Holographic":
-                    return DeviceFamily.Holographic;
-                case "Windows.IoT":
-                    return DeviceFamily.IoT;
-                case "Windows.Team":
-                    return DeviceFamily.Team;
-                default:
-                    return DeviceFamily.Unidentified;
-            }
-        }
-
-        public static DeviceFamily DeviceFamily { get; }
+        DeviceFamily = RecognizeDeviceFamily(Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily);
     }
+
+    public static DeviceFamily DeviceFamily { get; }
+
+    private static DeviceFamily RecognizeDeviceFamily(string deviceFamily)
+    {
+        switch (deviceFamily)
+        {
+            case "Windows.Mobile":
+                return DeviceFamily.Mobile;
+            case "Windows.Desktop":
+                return DeviceFamily.Desktop;
+            case "Windows.Xbox":
+                return DeviceFamily.Xbox;
+            case "Windows.Holographic":
+                return DeviceFamily.Holographic;
+            case "Windows.IoT":
+                return DeviceFamily.IoT;
+            case "Windows.Team":
+                return DeviceFamily.Team;
+            default:
+                return DeviceFamily.Unidentified;
+        }
+    }
+}
 }
--- DeviceFamilySpecificViews/DeviceFamilyTriggers/DeviceFamilyStateTrigger.cs
+++ DeviceFamilySpecificViews/DeviceFamilyTriggers/DeviceFamilyStateTrigger.cs
@@ -16,26 +16,26 @@ namespace DeviceFamilyTriggers
 
     namespace WindowsStateTriggers
     {
-        /// <summary>
-        /// Trigger to differentiate between device families
-        /// </summary>
-        public class DeviceFamilyStateTrigger : StateTriggerBase
-        {
-            public static readonly DependencyProperty TargetDeviceFamilyProperty = DependencyProperty.Register(
-                "TargetDeviceFamily", typeof(DeviceFamily), typeof(DeviceFamilyStateTrigger), new PropertyMetadata(default(DeviceFamily), OnDeviceTypePropertyChanged));
+/// <summary>
+/// Trigger to differentiate between device families
+/// </summary>
+public class DeviceFamilyStateTrigger : StateTriggerBase
+{
+    public static readonly DependencyProperty TargetDeviceFamilyProperty = DependencyProperty.Register(
+        "TargetDeviceFamily", typeof(DeviceFamily), typeof(DeviceFamilyStateTrigger), new PropertyMetadata(default(DeviceFamily), OnDeviceTypePropertyChanged));
 
-            public DeviceFamily TargetDeviceFamily
-            {
-                get { return (DeviceFamily)GetValue(TargetDeviceFamilyProperty); }
-                set { SetValue(TargetDeviceFamilyProperty, value); }
-            }
+    public DeviceFamily TargetDeviceFamily
+    {
+        get { return (DeviceFamily)GetValue(TargetDeviceFamilyProperty); }
+        set { SetValue(TargetDeviceFamilyProperty, value); }
+    }
 
-            private static void OnDeviceTypePropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
-            {
-                var trigger = (DeviceFamilyStateTrigger)dependencyObject;
-                var newTargetDeviceFamily = (DeviceFamily)eventArgs.NewValue;
-                trigger.SetActive(newTargetDeviceFamily == DeviceFamilyHelper.DeviceFamily);
-            }         
-        }
+    private static void OnDeviceTypePropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+    {
+        var trigger = (DeviceFamilyStateTrigger)dependencyObject;
+        var newTargetDeviceFamily = (DeviceFamily)eventArgs.NewValue;
+        trigger.SetActive(newTargetDeviceFamily == DeviceFamilyHelper.DeviceFamily);
+    }         
+}
     }
 }
