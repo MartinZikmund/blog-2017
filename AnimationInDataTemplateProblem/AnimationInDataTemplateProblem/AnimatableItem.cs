88471ac2543ff74a177adaeee90c5d24dbab77ba
Martin Zikmund <martinzikmund@live.com>
2017-04-10 00:47:45 +0200

    Animation in data template problem (differs in 14393 and 15063)

---
+++ AnimationInDataTemplateProblem/AnimationInDataTemplateProblem/AnimatableItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AnimationInDataTemplateProblem.Annotations;
+
+namespace AnimationInDataTemplateProblem
+{
+    public class AnimatableItem : INotifyPropertyChanged
+    {
+        private bool _isAnimating = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsAnimating
+        {
+            get { return _isAnimating; }
+            set
+            {
+                _isAnimating = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AnimationInDataTemplateProblem/AnimationInDataTemplateProblem/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AnimationInDataTemplateProblem
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public ObservableCollection<AnimatableItem> Items { get; } =
+            new ObservableCollection<AnimatableItem>()
+            {
+                new AnimatableItem(),
+                new AnimatableItem(),
+                new AnimatableItem(),
+                new AnimatableItem()
+            };
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Items[2].IsAnimating = !Items[2].IsAnimating;
+        }
+    }
+}
