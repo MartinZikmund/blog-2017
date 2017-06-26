1393edbad9a311880178cd9f48ccfa503c77d36e
Martin Zikmund <martinzikmund@live.com>
2017-06-26 18:13:02 +0200

    XAML field modifier directive

---
+++ XamlFieldModifierDirective/XamlFieldModifierDirective.UWP/OutsideAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlFieldModifierDirective.UWP
+{
+    public static class OutsideAccess
+    {
+        public static void ChangeTexts(MainPage page)
+        {
+            //page.InaccessibleTextBlock.Text = "Accessed"; - does not work
+            page.AccessibleTextBlock.Text = "Accessed";
+        }
+    }
+}
---
+++ XamlFieldModifierDirective/XamlFieldModifierDirective.WPF/OutsideAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlFieldModifierDirective.WPF
+{
+    public static class OutsideAccess
+    {
+        public static void ChangeTexts(MainWindow window)
+        {
+            window.InaccessibleTextBlock.Text = "Accessed";
+            window.AccessibleTextBlock.Text = "Accessed";
+        }
+    }
+}
