40bc2e290e49b6d8b0ed620d9233a183f81bbbbc
Martin Zikmund <martinzikmund@live.com>
2017-07-08 01:06:53 +0200

    Left command bar commands

---
+++ LeftCommandBarCommands/LeftCommandBarCommands/InverseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace LeftCommandBarCommands
+{
+    public class InverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+    }
+}
