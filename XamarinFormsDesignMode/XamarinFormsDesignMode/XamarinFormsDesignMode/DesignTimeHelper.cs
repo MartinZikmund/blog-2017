c422b7ee5cfdeec1d0b68c2559aa5474233375f8
Martin Zikmund <martinzikmund@live.com>
2017-07-20 16:24:44 +0200

    Xamarin.Forms Previwer Design mode check

---
+++ XamarinFormsDesignMode/XamarinFormsDesignMode/XamarinFormsDesignMode/DesignTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormsDesignMode
+{
+    public static class DesignTimeHelper
+    {
+        public static bool DesignModeOn { get; private set; }
+#if !RELEASE
+              =  true; //design mode is by default false in Release, just to be sure :-)
+#endif
+
+        public static void TurnOffDesignMode()
+        {
+            DesignModeOn = false;
+        }
+    }
+}
---
+++ XamarinFormsDesignMode/XamarinFormsDesignMode/XamarinFormsDesignMode/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsDesignMode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (DesignTimeHelper.DesignModeOn)
+            {
+                DescriptionLabel.Text = "Code was executed.";
+            }
+        }
+    }
+}
